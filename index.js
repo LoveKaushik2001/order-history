4c79d3564a1008be096a6b53698f79c1359c5388
LoveKaushik2001 <lovekaushik2001@gmail.com>
2025-02-16 18:24:59 +0530

    adding path variable

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
+const path = require('path');
 const { google } = require("googleapis");
 require("dotenv").config();
 const app = express();
+app.set('views', path.join(__dirname, 'views'));
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 
